90ace737bb34b545622f79694a26322d42226b57
José Antonio Quiñonero Gris <jose.aqg.universidad@gmail.com>
2023-02-19 14:22:17 +0100

    Commit antes de retomarlo tras carnaval

--- part-4-Intensive-course/Pere/homework/exercise-2/modules/Huckel_method.f90
+++ part-4-Intensive-course/Pere/homework/exercise-2/modules/Huckel_method.f90
@@ -7,6 +7,36 @@ module Huckel_method
     implicit none
     contains
 
+    subroutine d_vec(u, v, d)
+        implicit none
+        real(kind=8), dimension(:), intent(in) :: u, v
+        real(kind=8), intent(out) :: d
+        !
+        integer :: i, n, m 
+        real(kind=8) :: suma 
+        !
+        ! Get size of the vectors
+        !
+        n = size(u, dim=1)
+        m = size(v, dim=1)
+        !
+        ! Check that they are the same size
+        !
+        if (n /= m) stop &
+        & 'ERROR Huckel_method.f90 d_vec: Different size vectors'
+        !
+        ! Else, compute the distance
+        !
+        suma = 0.0_8
+        dli: do i = 1, n
+            suma = suma + ( v(i) - u(i) )**2
+        end do dli
+        !
+        d = sqrt(suma)
+        !
+        return
+    end subroutine d_vec 
+
     function norm_vec(v) result(v_norm)
         !
         ! Function that normalizes to one the vector v (real)
@@ -36,6 +66,51 @@ module Huckel_method
         return
     end function norm_vec
 
+    subroutine distance_matrix(xyz_mat, distance_mat)
+        implicit none
+        real(kind=8), dimension(:,:), intent(in) :: xyz_mat
+        real(kind=8), dimension(:,:), allocatable, intent(out) :: distance_mat
+        !
+        
+        !
+        return
+    end subroutine distance_matrix
+
+    subroutine Huckel_matrix(n, alpha, beta, H)
+        implicit none
+        integer, intent(in) :: n
+        real(kind=8), intent(in) :: alpha, beta 
+        real(kind=8), dimension(:,:), allocatable, intent(out) :: H
+        !
+        integer :: i, j, ierr
+        !
+        ! For the Hückel approximation
+        !
+        ! S_{ij} = delta_{ij}
+        !
+        ! the Hamiltonian matrix H reads
+        !             -
+        !            | alpha    if i = j
+        ! H_{i,j} = -  beta     if i,j adjacent
+        !            | 0        otherwise
+        !             -
+        allocate(H(n,n), stat=ierr)
+        if (ierr .ne. 0) stop &
+            & 'ERROR Huckel_method.f90 huckel_matrix: Error in allocation of H'
+        ! Initialize H
+        H = 0.0_8
+        dli: do i = 1, n
+            dlii: do j = i, n
+                if (i == j) H(i,j) = alpha
+                if (abs(i-j) == 1) H(i,j) = beta
+                ! Construct the rest of the matrix from symmetry
+                H(j,i) = H(i,j)
+            end do dlii
+        end do dli
+        !
+        return
+    end subroutine Huckel_matrix 
+        
     subroutine sort_evals_evec(ev, v)
         implicit none
         real(kind=8), dimension(:), intent(inout) :: ev
@@ -81,41 +156,6 @@ module Huckel_method
         return
     end subroutine sort_evals_evec 
 
-    subroutine Huckel_matrix(n, alpha, beta, H)
-        implicit none
-        integer, intent(in) :: n
-        real(kind=8), intent(in) :: alpha, beta 
-        real(kind=8), dimension(:,:), allocatable, intent(out) :: H
-        !
-        integer :: i, j, ierr
-        !
-        ! For the Hückel approximation
-        !
-        ! S_{ij} = delta_{ij}
-        !
-        ! the Hamiltonian matrix H reads
-        !             -
-        !            | alpha    if i = j
-        ! H_{i,j} = -  beta     if i,j adjacent
-        !            | 0        otherwise
-        !             -
-        allocate(H(n,n), stat=ierr)
-        if (ierr .ne. 0) stop &
-            & 'ERROR Huckel_method.f90 huckel_matrix: Error in allocation of H'
-        ! Initialize H
-        H = 0.0_8
-        dli: do i = 1, n
-            dlii: do j = i, n
-                if (i == j) H(i,j) = alpha
-                if (abs(i-j) == 1) H(i,j) = beta
-                ! Construct the rest of the matrix from symmetry
-                H(j,i) = H(i,j)
-            end do dlii
-        end do dli
-        !
-        return
-    end subroutine Huckel_matrix 
-        
     subroutine Huckel_evals_evecs(ev, v)
         implicit none
         real(kind=8), dimension(:), intent(out) :: ev
--- part-4-Intensive-course/Pere/homework/exercise-2/modules/Mulliken_population_analysis.f90
+++ part-4-Intensive-course/Pere/homework/exercise-2/modules/Mulliken_population_analysis.f90
@@ -2,9 +2,26 @@
 ! | Author: Jose Antonio Quinonero Gris         |
 ! | Creation date: Thursday 22:17:03 09/02/2023 |
 ! +---------------------------------------------+
-
-module Mulliken_pop_analysis
-    implicit none
-    contains
-        
-end module Mulliken_pop_analysis
+!
+! module Mulliken_pop_analysis
+!     implicit none
+!     contains
+!
+!     subroutine Mulliken_population_matrix(nMO, C, S, P)
+!         !
+!         ! nMO: number of molecular orbitals
+!         ! C: coefficient matrix of the AO's for each MO (columns)
+!         ! S: overlap matrix between AO's
+!         ! P: Mulliken population matrix
+!         !
+!         implicit none
+!         integer, intent(in) :: nMO 
+!         real(kind=8), dimension(:,:), intent(in) :: C, S
+!         real(kind=8), dimension(:,:), allocatable, intent(out) :: P
+!         !
+!         
+!         !
+!         return
+!     end subroutine Mulliken_population_matrix 
+!         
+! end module Mulliken_pop_analysis
